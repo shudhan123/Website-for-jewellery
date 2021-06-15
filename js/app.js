01d301b2fb15e77553f0b2d2a1cdb2ec7b9e4f3b
shudhan123 <85920129+shudhan123@users.noreply.github.com>
2021-06-15 10:50:47 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,85 @@
+// dropdown menu show or hide
+function dropDownMenuControl() {
+	var dropDown = document.querySelector(".dropdown");
+	if (dropDown.style.display == "none"){
+		dropDown.style.display = "flex";
+		dropDown.classList.remove("bounceOutUp");
+		dropDown.classList.add("bounceInDown");
+	}
+	else if (dropDown.style.display == "flex"){
+		dropDown.classList.remove("bounceInDown");
+		dropDown.classList.add("bounceOutUp");
+		setTimeout(function(){
+			dropDown.style.display = "none";
+		}, 800);
+	}
+}
+
+// navbar size
+function changeNavSize() {
+	var logo = document.querySelector(".nav-bar img");
+	var dropDown = document.querySelector(".dropdown");
+	if (window.scrollY > 200) {
+		var w = 10*3.404 + "%";
+		logo.style.width = w;
+		var h = 10*1 + "%";
+		logo.style.height = h;
+		dropDown.style.paddingTop = "44px";
+	}
+	else {
+		logo.style.width = 18*3.404 + "%";
+		logo.style.height = 18*1 + "%";
+		dropDown.style.paddingTop = "64px";
+	}
+}
+
+//go to top button
+var goTopBtn = document.getElementById("goTopBtn");
+function gotoTopShoworHide(){
+	var goTopBtn = document.getElementById("goTopBtn");
+	if (window.pageYOffset < 500){
+		goTopBtn.classList.add("fadeOut");
+		setTimeout(function(){
+			goTopBtn.style.display = "none";
+		}, 100);
+		goTopBtn.classList.remove("fadeIn");
+	}
+	else{
+		goTopBtn.style.display = "flex";
+		goTopBtn.classList.add("fadeIn");
+		goTopBtn.classList.remove("fadeOut");
+	}
+}
+function gotoTop(){
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+// hide menu on click on body area other than the menu
+document.addEventListener("mouseup", function(e){
+    var container = document.querySelector(".dropdown");
+    if (!container.contains(e.target)){
+        container.style.display = "none";
+    }
+}.bind(this));
+
+// show or hide cart list
+function showHideCartList(){
+    var cartlist = document.querySelector(".cart-list");
+    if (cartlist.style.display == ""){
+        cartlist.style.display = "flex";
+    }
+    else if (cartlist.style.display == "flex"){
+		cartlist.style.display = "";
+    }
+}
+
+window.addEventListener("scroll", changeNavSize, false);
+window.addEventListener("scroll", gotoTopShoworHide, false);
+
+// current year
+var date = new Date();
+var year = date.getFullYear();
+var copyright = document.querySelector("#copyright");
+copyright.innerHTML = "&copy; " + year + " Rathinam Gold & Diamond Pvt LTD";
+
---
+++ js/carousel.js
@@ -0,0 +1,43 @@
+var j;
+var currCar = "", preCar = "";
+autoChange(1);
+
+function autoChange(current){
+    if (current === undefined){
+    }
+    else{
+        j = current;
+    }
+    for (i = 1; i < 3; i++){
+        currCar = document.getElementById("car-" + i);
+        currCar.classList.remove("slideInRight");
+        currCar.style.zIndex = "-3";
+    }
+    if (j - 1 == 0){
+        preCar = document.getElementById("car-" + 2);    
+    }
+    else{
+        preCar = document.getElementById("car-" + (j-1));    
+    }
+    preCar.style.zIndex = "-2";
+    
+    currCar = document.getElementById("car-" + j);
+    currCar.style.zIndex = "-1";
+    currCar.style.display = "block";
+    currCar.classList.add("slideInRight");
+    var switches = document.querySelectorAll(".switches .switch");
+    var i, s = switches.length;
+    for (i = 0; i < s; i++){
+        switches[i].classList.remove("active");
+    }
+    switches[j-1].classList.add("active");
+    j++;
+    if (j > 2){
+        j = 1;
+    }
+    var timer = setTimeout(autoChange, 5000);
+}
+
+function changePic(id, btn){
+    setTimeout(autoChange, 5000, id);
+}
